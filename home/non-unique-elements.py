d4923aae801df17a18dafc5ad80b388de1105c48
chlos <baphonet@gmail.com>
2014-04-08 13:28:33 +0400

    home/non-unique-elements.py:  __main__ check added

--- home/non-unique-elements.py
+++ home/non-unique-elements.py
@@ -9,7 +9,8 @@
 def checkio(data):
     return [e for e in data if data.count(e) > 1]
 
-assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
-assert checkio([1, 2, 3, 4, 5]) == []
-assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
-assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
+if __name__ == '__main__':
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
+    assert checkio([1, 2, 3, 4, 5]) == []
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
