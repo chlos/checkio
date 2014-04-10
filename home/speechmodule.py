45f7c0b8fd7beb89c1dea6438686990d2248ae1d
chlos <baphonet@gmail.com>
2014-04-10 14:20:25 +0400

    home/speechmodule.py added (just a blank template)

---
+++ home/speechmodule.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+FIRST_TEN = ["one", "two", "three", "four", "five", "six", "seven",
+             "eight", "nine"]
+SECOND_TEN = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+              "sixteen", "seventeen", "eighteen", "nineteen"]
+OTHER_TENS = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+              "eighty", "ninety"]
+HUNDRED = "hundred"
+
+def checkio(number):
+
+    #replace this for solution
+    return 'string representation of n'
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(4) == 'four', "1st example"
+    assert checkio(133) == 'one hundred thirty three', "2nd example"
+    assert checkio(12) == 'twelve', "3rd example"
+    assert checkio(101) == 'one hundred one', "4th example"
+    assert checkio(212) == 'two hundred twelve', "5th example"
+    assert checkio(40) == 'forty', "6th example"
+    assert not checkio(212).endswith(' '), "Don't forget strip whitespaces " \
+                                                "at the end of string"
