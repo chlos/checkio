177a5825fa2c6eb6e8f85dc7b4f32c08606e2313
chlos <baphonet@gmail.com>
2014-04-10 14:10:51 +0400

    home/x-o-referee.py: x-o-referee functionality implemented

--- home/x-o-referee.py
+++ home/x-o-referee.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 
 def checkio(game_result):
-    return "D" or "X" or "O"
+    ln = len(game_result)
+    lines = [list(line) for line in game_result]
+    cols = [[line[col] for line in game_result] \
+                        for col in range(0, ln)]
+    diags = [[], []]
+    for i in range(0, ln):
+        diags[0].append(game_result[i][i])
+        diags[1].append(game_result[i][ln - 1 - i])
+    for var in lines + cols + diags:
+        if len(set(var)) == 1:
+            winner = var[0]
+            if winner in ["X", "x", "O", "o"]:
+                return winner
+    return "D"
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
@@ -21,3 +34,7 @@ if __name__ == '__main__':
         "O.X",
         "XX.",
         "XOO"]) == "X", "Xs wins again"
+    assert checkio([
+        "...",
+        "XXX",
+        "OO."]) == "X", "Xs wins, not dots"
