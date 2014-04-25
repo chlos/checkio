719e57193d1323f4f25575ee8192f003d107080c
chlos <baphonet@gmail.com>
2014-04-25 16:26:07 +0400

    home/roman-numerals.py added (just a blank template)

---
+++ home/roman-numerals.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def checkio(data):
+
+    #replace this for solution
+    return ""
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
