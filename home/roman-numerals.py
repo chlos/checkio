a49c8e118df04ba85d24c274dd197dd603d64d35
chlos <baphonet@gmail.com>
2014-05-06 13:29:49 +0400

    home/roman-numerals.py: solution added

--- home/roman-numerals.py
+++ home/roman-numerals.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
 def checkio(data):
-
-    #replace this for solution
-    return ""
+    ints = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    romans = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+    map_int_rom = tuple(zip(ints, romans))
+    result = ''
+    for i, roman in map_int_rom:
+        multiplier = data // i
+        result += roman * multiplier
+        data -= i * multiplier
+    return result
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
