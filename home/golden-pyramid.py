efe347f03c702c0a57941e4642e5ad2ad80f2576
chlos <baphonet@gmail.com>
2014-07-02 20:34:10 +0400

    golden-pyramid.py: solution added

--- home/golden-pyramid.py
+++ home/golden-pyramid.py
@@ -5,8 +5,26 @@ def count_gold(pyramid):
     Return max possible sum in a path from top to bottom
     """
 
-    #replace this for solution
-    return 0
+    # returns 0 if you get out of pyramid
+    def get_cost(costs, i, j):
+        if j < 0 or i < 0:
+            return 0
+        try:
+            return costs[i][j]
+        except IndexError:
+            return 0
+
+    costs = []  # max costs of i,j-position
+    for i in range(0, len(pyramid)):
+        costs.append([])
+        for j in range(0, len(pyramid[i])):
+            if i == j == 0:
+                costs[i].append(pyramid[i][j])
+            else:
+                cij = pyramid[i][j] + max(get_cost(costs, i - 1, j - 1),
+                                            get_cost(costs, i - 1, j))
+                costs[i].append(cij)
+    return max(costs[-1])
 
 
 if __name__ == '__main__':
