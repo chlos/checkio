e3a091064aa48f9562fc50e87b4b7e8403e437fb
chlos <baphonet@gmail.com>
2014-07-09 19:34:32 +0400

    home/find-friends.py: solution added

--- home/find-friends.py
+++ home/find-friends.py
@@ -1,5 +1,28 @@
+def merge(lsts):
+  sets = [set(lst) for lst in lsts if lst]
+  merged = 1
+  while merged:
+    merged = 0
+    results = []
+    while sets:
+      common, rest = sets[0], sets[1:]
+      sets = []
+      for x in rest:
+        if x.isdisjoint(common):
+          sets.append(x)
+        else:
+          merged = 1
+          common |= x
+      results.append(common)
+    sets = results
+  return sets
+
 def check_connection(network, first, second):
-    return True or False
+    lists = [friends.split('-') for friends in network]
+    for st in merge(lists):
+        if (first in st) and (second in st):
+            return True
+    return False
 
 
 if __name__ == '__main__':
@@ -16,3 +39,7 @@ if __name__ == '__main__':
         ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
          "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
         "dr101", "sscout") == False, "I don't know any scouts."
+    assert check_connection(
+        ("nikola-robin","batman-nwing","mr99-batman", "mr99-robin",
+        "dr101-out00","out00-nwing",),
+        "dr101","mr99") == True
